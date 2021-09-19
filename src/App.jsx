8c569dd02e06cbe83b70f0032eb6cc5a6e3750b8
saagarshah99 <saagarshahs@hotmail.com>
2021-09-19 14:27:58 +0100

    Tidying up JS

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,46 @@ import CardList from './components/CardList/CardList';
 import SearchBox from "./components/SearchBox/SearchBox";
 import FiltersList from "./components/SearchBox/FiltersList/FiltersList";
 
+// checking multiple keys in object for search query match
+const checkForMatch = (searchQuery, {name, description, first_brewed, ph, abv}) => {
+  const keys = [
+    name.toLowerCase(), 
+    description.toLowerCase(),
+    first_brewed,
+    ph.toString(),
+    abv.toString(),
+  ];
+
+  
+
+  let numberOfMatches = 0;
+  for (let i = 0; i < keys.length; i++) {
+    if(keys[i].includes(searchQuery)) {
+      numberOfMatches++;
+    }
+  }
+
+  return numberOfMatches > 0;
+}
+
+// checking beers array for any search matches and applying filters if any selected
+const getFilteredBeers = (searchQuery, filter) => {
+    return beers.filter(beer => {
+      const {first_brewed, ph, abv} = beer;
+      const matchFound = checkForMatch(searchQuery, beer);
+  
+      if(filter === "ABV") return matchFound && abv > 6;
+      
+      else if(filter === "Classic") {
+        return matchFound && parseInt(first_brewed.split("/")[1]) <= 2010;
+      }
+  
+      else if(filter === "Acidic") return matchFound && ph < 4;
+      
+      return matchFound;
+    });
+}
+
 function App() {
 
   // creating and handling states for searching and filtering
@@ -14,27 +54,6 @@ function App() {
   const handleSearch = event => setSearchQuery(event.target.value.toLowerCase());
   const [filter, setFilter] = useState("");
   const handleFilter = event => setFilter(event.target.value);
-  
-  // checking beers array for any search matches and applying filters if any selected
-  const filteredBeers = beers.filter(beer => {
-    const matchFound = 
-      beer.name.toLowerCase().includes(searchQuery) || 
-      beer.description.toLowerCase().includes(searchQuery) ||
-      beer.first_brewed.includes(searchQuery) ||
-      beer.ph.toString().includes(searchQuery) ||
-      beer.abv.toString().includes(searchQuery)
-    ;
-
-    if(filter === "ABV") return matchFound && beer.abv > 6;
-    
-    else if(filter === "Classic") {
-      return matchFound && parseInt(beer.first_brewed.split("/")[1]) <= 2010;
-    }
-
-    else if(filter === "Acidic") return matchFound && beer.ph < 4;
-    
-    return matchFound;
-  });
   
   return (
     <div className="App">
@@ -45,7 +64,7 @@ function App() {
         
         <main>
             <div className="card-list">
-                <CardList beerArr={filteredBeers} />
+                <CardList beerArr={getFilteredBeers(searchQuery, filter)} />
             </div>
         </main>
     </div>
