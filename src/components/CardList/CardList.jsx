d64e029472fdff9287af31b0c4886da53f7e4dfd
saagarshah99 <saagarshahs@hotmail.com>
2021-09-20 22:43:58 +0100

    Shuffling beers before outputting them

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import "./CardList.scss";
 import Card from './Card/Card';
 
-// receiving array and mapping through it to output card for each beer
-const CardList = ({ beerArr }) => beerArr.map((beer) => <Card beer={beer} />);
+// receiving array, shuffling it and mapping through it to output card for each beer
+const CardList = ({ beerArr }) => {
+    const shuffledBeers = beerArr.sort(() => Math.random() - 0.5);
+    
+    return shuffledBeers.map((beer) => <Card beer={beer} />);
+}
 
 export default CardList;
